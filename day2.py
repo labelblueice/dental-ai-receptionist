1f4ecfd895b8014edd23f64cbfb01c2454d40885
Bhuwan <221832209+labelblueice@users.noreply.github.com>
2026-07-03 17:02:14 +0545

    Day2 - Dental AI

---
+++ day2.py
@@ -0,0 +1,17 @@
+# Dental AI
+
+clinic_name = "Himal Dental Hospital"
+
+doctor_name = "Dr. Sharma"
+
+patient_name = "Ram Sharma"
+
+age = 25
+
+phone = "9840234565"
+
+print(clinic_name)
+print(doctor_name)
+print(patient_name)
+print(age)
+print(phone)
